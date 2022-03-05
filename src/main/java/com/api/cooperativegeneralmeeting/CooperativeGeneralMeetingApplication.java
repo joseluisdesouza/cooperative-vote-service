d28e31adefc820a155b0cbcde99f5626a36d48b1
josesouza <joseluisdesouza9492@gmail.com>
2022-03-05 14:47:21 -0300

    project start first commit

---
+++ src/main/java/com/api/cooperativegeneralmeeting/CooperativeGeneralMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.api.cooperativegeneralmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativeGeneralMeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativeGeneralMeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cooperativegeneralmeeting/CooperativeGeneralMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cooperativegeneralmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeGeneralMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
